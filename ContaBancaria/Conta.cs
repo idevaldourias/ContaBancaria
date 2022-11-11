51d7b73f4687c9ccc7803c93044721d68cef8e0a
idevaldourias <idevaldorodrigues@gmail.com>
2022-11-11 12:08:14 -0300

    Aula de orientação à objetos em c sharp

---
+++ ContaBancaria/Conta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    internal class Conta
+    {
+        private string _nome;
+        public int ContaNumero { get; private set; }
+        public double Saldo { get; private set; }
+        public Conta(int contaNumero, string nome)
+        {
+            ContaNumero = contaNumero;
+            Nome = nome;
+        }
+        public Conta(int contaNumero, string nome, double depositoInicial) : this(contaNumero, nome)
+        {
+            Deposito(depositoInicial);
+        }
+        public string Nome
+        {
+            get { return _nome; } 
+            set { 
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+        public void Saque(double valor)
+        {
+            Saldo -= (valor + 5.0);
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + ContaNumero + ", Titular: " + Nome + ", Saldo: R$" + Saldo.ToString("F2") + ".";
+        }
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+
+            Console.Write("Entre o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char depositobool = char.Parse(Console.ReadLine());
+
+            if (depositobool == 's' || depositobool == 'S') {
+                Console.Write("Entre o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+
+                conta = new Conta(numeroConta, nome, depositoInicial);
+            } 
+            else
+            {
+                conta = new Conta(numeroConta, nome);
+            }
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine());
+            conta.Deposito(quantia);
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine());
+            conta.Saque(quantia);
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+        }
+    }
+}
